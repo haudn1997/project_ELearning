0f1e53870a9e787224bc844161d8dd16db2c90cf
haudn1997 <tanhau1910@gmail.com>
2018-03-09 11:45:44 +0700

    init_projet

---
+++ resources/lang/en/message.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+	    'errors' => 'Whoops! Something went wrong!',
+	    'task' => 'Task',
+	    'Add_task' => 'Add task',
+	    'titles' => 'Laravel Quickstart - Basic',
+	    'Current_Tasks' => ' Current Tasks',
+	    'Add' => 'Add Task',
+	    'delete' => 'Delete',
+	    'add_fails' => 'Add Fails',
+	    'delete_fails' => 'Delete Failed! Please try again'
+	];
+?>
+
+
